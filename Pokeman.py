8d15f4d9ac3163fe517a1958a3029fd4f763145a
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2020-11-20 11:08:28 +0100

    Convert the program to represent the pokemon as an object and isolate the unrelated parts

--- Pokeman.py
+++ Pokeman.py
@@ -1,46 +1,115 @@
+#!/usr/bin/env python3
+
 from bs4 import BeautifulSoup
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import requests
 import random
-import json
 import urllib.parse
 import re
+import os
+import json
+import yaml
 
-pokedex = 'https://www.pokemon.com/us/pokedex/'
-pokeimg = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/{}.png'
-slackurl_photo = 'https://slack.com/api/users.setPhoto?token={}&pretty=1'
-slackurl_status = 'https://slack.com/api/users.profile.set?token={}&profile={}&pretty=1'
-token = ''
 
-r = requests.get(pokedex)
-soup = BeautifulSoup(r.text, 'html.parser')
+class Pokemon:
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
 
-pokedex_re = re.compile('^National Pokédex$')
-pokemon_re = re.compile('(\d+) - (\w+)')
+    def __str__(self):
+        return f'{self.name} - {self._padded_number()}'
 
-pokenumbers = []
-for header in soup.find_all('h2', text=pokedex_re):
-    ul_tag = header.findNext('ul')
-    for a_tag in ul_tag.find_all('a'):
-        match = re.match(pokemon_re, a_tag.text)
-        number = match.group(1)
-        name = match.group(2)
-        pokenumbers.append((number, name))
+    def img_url(self):
+        return f'https://assets.pokemon.com/assets/cms2/img/pokedex/full/{self._padded_number()}.png'
 
-pokemon = random.sample(pokenumbers, 1).pop()
-padded_number = f'{int(pokemon[0]):03}'
-pokename = pokemon[1]
-pokeurl = pokeimg.format(padded_number)
-pokepng = requests.get(pokeurl).content
+    def png(self):
+        name = 'pokemon' + os.path.basename(self.img_url())
+        png = requests.get(self.img_url()).content
+        return name, png
 
-m = MultipartEncoder(fields={'image': ('pokemon{}.png'.format(padded_number), pokepng, 'image/png')})
-headers = {'Content-Type': m.content_type}
-requests.post(slackurl_photo.format(token), headers=headers, data=m)
+    def _padded_number(self):
+        return f'{int(self.number):03}'
 
-pokestatus = '{} - {}'.format(padded_number, pokename)
-profile = {'status_text': pokestatus}
-profile_json = json.dumps(profile)
-encoded_json = urllib.parse.quote(profile_json)
-requests.post(slackurl_status.format(token, encoded_json))
 
-print('Transformed to ' + pokestatus)
+def pokemen():
+    pokedex = 'https://www.pokemon.com/us/pokedex/'
+
+    request = requests.get(pokedex)
+    soup = BeautifulSoup(request.text, 'html.parser')
+
+    pokedex_re = re.compile('^National Pokédex$')
+    pokemon_re = re.compile('(\d+) - (\w+)')
+
+    pokemen = []
+    for header in soup.find_all('h2', text=pokedex_re):
+        ul_tag = header.findNext('ul')
+        for a_tag in ul_tag.find_all('a'):
+            match = re.match(pokemon_re, a_tag.text)
+            number = match.group(1)
+            name = match.group(2)
+            pokemen.append(Pokemon(name, number))
+
+    return pokemen
+
+
+def pokemon_pick():
+    return random.sample(pokemen(), 1).pop()
+
+
+def change_slack_photo(token, name, png, dryrun=False):
+    slackurl_photo = f'https://slack.com/api/users.setPhoto?token={token}&pretty=1'
+    m = MultipartEncoder(fields={'image': (name, png, 'image/png')})
+    headers = {'Content-Type': m.content_type}
+    url = slackurl_photo.format(token)
+    if dryrun:
+        print('Change Slack Photo')
+        print(url)
+    else:
+        requests.post(url, headers=headers, data=m)
+
+
+def change_slack_status(token, status, dryrun=False):
+    profile = {'status_text': status}
+    profile_json = json.dumps(profile)
+    encoded_json = urllib.parse.quote(profile_json)
+    payload = f'https://slack.com/api/users.profile.set?token={token}&profile={encoded_json}&pretty=1'
+    if dryrun:
+        print('Change Slack Status')
+        print(payload)
+    else:
+        requests.post(payload)
+
+
+def post_slack_qotd(hook, name, url, dryrun=False):
+    slackurl_hook = hook
+    r = requests.get('http://quotes.rest/qod?category=management')
+    json_data = json.loads(r.text)
+    if json_data:
+        quote = json_data['contents']['quotes'][0]['quote']
+        author = json_data['contents']['quotes'][0]['author']
+
+    payload = {'text': '{} finds this quote inspiring...'.format(name), 'attachments': [{'author_name': author,
+                                                                                         'thumb_url': url,
+                                                                                         'text': quote}]}
+    if dryrun:
+        print('Post Slack QOTD')
+        print(slackurl_hook, payload)
+    else:
+        requests.post(slackurl_hook, json=payload)
+
+
+with open('config.yml') as f:
+    config = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+token = config.get('token')
+qotd_hook = config.get('qotd_hook')
+
+pokemon = pokemon_pick()
+
+if token and len(token) > 0:
+    name, png = pokemon.png()
+    change_slack_photo(token, name, png)
+    change_slack_status(token, str(pokemon))
+
+if qotd_hook and len(qotd_hook) > 0:
+    post_slack_qotd(qotd_hook, pokemon.name, pokemon.img_url())
